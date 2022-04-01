7002f4d4573c3cf4aaf24b23ca95b94f053d6d30
Guizboule <guiiillaume44@gmail.com>
2022-04-01 23:03:59 +0700

    L3

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data(){
         return{
             product: 'Socks',
-            description: 'My super socks to run faster !'
+            description: 'My super socks to run faster !',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.fr/'
         }
     }
 })
